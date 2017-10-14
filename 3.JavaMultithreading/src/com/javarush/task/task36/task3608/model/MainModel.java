c1ca432896e3fa431ace35ab8ca3ff74d9acfc79
the-markussohn <markus.justinas@gmail.com>
2017-10-14 23:33:39 +0300

    MVC 4

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+/**
+ * Created by Justinas on 2017-10-14.
+ */
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> usersBetweenLevels = userService.getUsersBetweenLevels(1, 100);
+        modelData.setUsers(usersBetweenLevels);
+    }
+}
