f1918fb0ed3bfdef90d39996795c2f5161e6c283
the-markussohn <markus.justinas@gmail.com>
2017-10-15 02:50:00 +0300

    Refactor 2

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Soldier.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task29.task2909.human;
+
+/**
+ * Created by Justinas on 2017-10-15.
+ */
+public class Soldier extends Human {
+
+    public Soldier(String name, int age) {
+        super(name, age);
+    }
+
+    public void fight() {
+    }
+
+    public void live() {
+        fight();
+    }
+}
