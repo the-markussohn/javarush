2e2ca79d48a7734abd9f5cf76c9e0eeae8473135
the-markussohn <markus.justinas@gmail.com>
2017-10-14 22:51:42 +0300

    MVC 1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Justinas on 2017-10-14.
+ */
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> list = new ArrayList<>();
+        list.add(new User("A", 1, 1));
+        list.add(new User("B", 2, 1));
+        list.add(new User("C", 3, 1));
+        modelData.setUsers(list);
+    }
+}
