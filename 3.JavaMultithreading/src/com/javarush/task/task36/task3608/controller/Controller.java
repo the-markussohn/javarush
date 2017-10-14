a566d935b0abf4a2b872b538c99e1a50d9873f3f
the-markussohn <markus.justinas@gmail.com>
2017-10-14 23:26:55 +0300

    MVC 3

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,12 +1,15 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.model.ModelData;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 /**
  * Created by Justinas on 2017-10-14.
  */
 public class Controller {
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -14,5 +17,10 @@ public class Controller {
 
     public void onShowAllUsers() {
         model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 }
