396f9381282bb58cad455203691821fdfa7c2b41
the-markussohn <markus.justinas@gmail.com>
2017-10-16 01:18:40 +0300

    Chat 1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task30.task3008;
+
+/**
+ * Created by Justinas on 2017-10-16.
+ */
+public enum MessageType {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task30.task3008;
+
+/**
+ * Created by Justinas on 2017-10-16.
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
