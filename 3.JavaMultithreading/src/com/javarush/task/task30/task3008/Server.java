c2a3831314fbfa799bec6ba11ca7436fe5106fbd
the-markussohn <markus.justinas@gmail.com>
2017-10-16 02:09:29 +0300

    Chat 6

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,10 +1,35 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Justinas on 2017-10-16.
  */
 public class Server {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        int port = ConsoleHelper.readInt();
+        ServerSocket socket = new ServerSocket(port);
+        ConsoleHelper.writeMessage("Listening on port" + socket.getLocalPort());
+        try {
+            while (true) {
+                Socket accept = socket.accept();
+                new Handler(accept).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+        } finally {
+            socket.close();
+        }
     }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
 }
