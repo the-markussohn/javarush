73452d7249617fba4a2b952fb9e6447c78406885
the-markussohn <markus.justinas@gmail.com>
2017-10-17 01:12:49 +0300

    Chat 13

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  * Created by Justinas on 2017-10-17.
@@ -8,6 +14,38 @@ import com.javarush.task.task30.task3008.Connection;
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Enter server address:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Enter server port:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Enter your username:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Error!");
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread {
     }
