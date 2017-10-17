29c835aa23f51a57c850ad4b3797199dd88b6844
the-markussohn <markus.justinas@gmail.com>
2017-10-17 03:05:14 +0300

    Archiver 1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+/**
+ * Created by Justinas on 2017-10-17.
+ */
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter filepath to the archive:");
+        String archiver = reader.readLine();
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(archiver));
+        System.out.println("Enter filepath to the file to be archived:");
+        String file = reader.readLine();
+        zipFileManager.createZip(Paths.get(file));
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task31.task3110;
+
+import java.nio.file.Path;
+
+/**
+ * Created by Justinas on 2017-10-17.
+ */
+public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception {
+
+    }
+}
