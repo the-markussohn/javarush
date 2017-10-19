7343c8737c91d0b550724b912dd7009580724d0f
the-markussohn <markus.justinas@gmail.com>
2017-10-20 00:34:47 +0300

    Archiver 2

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by Justinas on 2017-10-17.
@@ -14,5 +20,15 @@ public class ZipFileManager {
 
     public void createZip(Path source) throws Exception {
 
+        try (ZipOutputStream outputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             InputStream inputStream = Files.newInputStream(source)) {
+            ZipEntry entry = new ZipEntry(String.valueOf(source.getFileName()));
+            outputStream.putNextEntry(entry);
+            while (inputStream.available() > 0) {
+                outputStream.write(inputStream.read());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
