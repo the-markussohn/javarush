b9bb637cc33cd60dfa934963374edab93d68c7ad
the-markussohn <markus.justinas@gmail.com>
2017-10-20 00:40:13 +0300

    Archiver 3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Justinas on 2017-10-20.
+ */
+public class ConsoleHelper {
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return bufferedReader.readLine();
+    }
+
+    public static int readInt() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(bufferedReader.readLine());
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Operation.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task31.task3110;
+
+/**
+ * Created by Justinas on 2017-10-20.
+ */
+public enum Operation {
+    CREATE,
+    ADD,
+    REMOVE,
+    EXTRACT,
+    CONTENT,
+    EXIT
+}
